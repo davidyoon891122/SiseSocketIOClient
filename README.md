77380ff3b7c621a637dc5b87497ae9edcf3c6463
Davidyoon <davidyoon@koreainvestment.com>
2023-01-25 16:22:47 +0900

    Add a readme file
    * Added a Readme file
    * Connected on sise code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Socket io client 
+
+## 설명
+
+- socket io를 사용하는 웹 서버 테스트를 위한 클라이언트 프로그램
+
+## Library
+
+- socket.io-client
+
+## How to execute
+
+- node index.js
--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const socket = io('http://127.0.0.1:3000/sise')
 socket.on("connect", (receive) => {
     console.log("try to connect...")
 
-    socket.emit("service", "sise")
+    // socket.emit("service", "sise")
 
     socket.on('service', (message) => {
         console.log(`message from sise server : ${message}`)
     })
 
+    socket.emit("code", "samsung")
+
     socket.on('sise', (data) => {
         console.log(`sise: ${data}`)
-    } )
-
+    })
 })
