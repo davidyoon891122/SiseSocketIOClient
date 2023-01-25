b4d75633caad36d34a098f539bc168f263bc9028
Davidyoon <davidyoon@koreainvestment.com>
2023-01-25 12:54:57 +0900

    Modify connection
    * Connected to sise using namespace

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { io } = require("socket.io-client")
 
 console.log("Client started")
-const socket = io('http://127.0.0.1:3000')
+const socket = io('http://127.0.0.1:3000/sise')
 
-socket.on("connect", () => {
+socket.on("connect", (receive) => {
     console.log("try to connect...")
+
+    socket.emit("service", "sise")
+
+    socket.on('service', (message) => {
+        console.log(`message from sise server : ${message}`)
+    })
+
 })
